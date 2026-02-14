f3fc4dcb0a4a12ba0d3795dd97bcefff3e79b390
Jingchao Feng <fjch1997@outlook.com>
2026-02-14 13:12:32 -0800

    Handle magnet already exists in test

--- DelugionalTests/DelugeRpcTests.cs
+++ DelugionalTests/DelugeRpcTests.cs
@@ -6,6 +6,10 @@ using Delugional;
 using Delugional.Daemon;
 using Delugional.Rpc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DelugionalTests
 {
@@ -59,18 +63,37 @@ namespace DelugionalTests
             }
         }
 
+        private string ParseHashFromMagnetLink(string magnetLink)
+        {
+            const string HashPrefix = "urn:btih:";
+            int hashStartIndex = magnetLink.IndexOf(HashPrefix) + HashPrefix.Length;
+            int hashEndIndex = magnetLink.IndexOf('&', hashStartIndex);
+            if (hashEndIndex == -1)
+                hashEndIndex = magnetLink.Length;
+            return magnetLink.Substring(hashStartIndex, hashEndIndex - hashStartIndex);
+        }
+
+        private async Task<string> AddMagnetAsync(IDelugeRpc deluge)
+        {
+            var existingTorrents = await deluge.GetTorrentsStatusAsync(new Filter { Keywords = { ParseHashFromMagnetLink(Resources.MagnetLink1) } });
+            if (existingTorrents.Count > 0)
+            {
+                // Torrent already exists, return its ID.
+                return existingTorrents.Keys.First();
+            }
+            return await deluge.AddMagnetAsync(Resources.MagnetLink1);
+        }
+
         [TestMethod]
         public async Task GetTorrentStatus()
         {
             using (IDelugeRpc deluge = await daemon.OpenRpcAsync())
             {
                 await deluge.LoginAsync(user.Username, user.Password);
-
-                string torrentId = await deluge.AddMagnetAsync(Resources.MagnetLink1);
-
+                var torrentId = await AddMagnetAsync(deluge);
                 try
                 {
-                    IDictionary<string, object> status = await deluge.GetTorrentStatusAsync(torrentId, new []{BuiltInStatuses.Name, BuiltInStatuses.ActiveTime});
+                    IDictionary<string, object> status = await deluge.GetTorrentStatusAsync(torrentId, new[] { BuiltInStatuses.Name, BuiltInStatuses.ActiveTime });
                     
                     Assert.IsNotNull(status, "status != null");
                     Assert.AreEqual(2, status.Count, "status.Count == 2");
